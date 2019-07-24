d382c2d5b478f5af13bf46ab1e246d498db96b3c
Andrew H <ahennesen@gmail.com>
2019-07-24 14:58:48 -0400

    Updates

---
+++ precache-manifest.fbd81e7eabcd9c22d4460399ae047bfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2da9f3924d1aa227e040f3e4df92ef",
+    "url": "/Portfolio-2/index.html"
+  },
+  {
+    "revision": "98458f2b13c1914e1f49",
+    "url": "/Portfolio-2/static/css/main.b1619151.chunk.css"
+  },
+  {
+    "revision": "d8a6fdde8c0717a6c29a",
+    "url": "/Portfolio-2/static/js/2.2ac5de18.chunk.js"
+  },
+  {
+    "revision": "98458f2b13c1914e1f49",
+    "url": "/Portfolio-2/static/js/main.dc1e4692.chunk.js"
+  },
+  {
+    "revision": "bc0f7ef3dae4eadf1d05",
+    "url": "/Portfolio-2/static/js/runtime~main.07b85818.js"
+  }
+]);
